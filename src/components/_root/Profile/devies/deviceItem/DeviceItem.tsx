cee3c7d66ddfa261989625070438d7a8c37caec0
wolffanil2 <wolffanil@mail.ru>
2025-06-24 18:07:01 +0400

    fix devices 2

--- src/components/_root/Profile/devies/deviceItem/DeviceItem.tsx
+++ src/components/_root/Profile/devies/deviceItem/DeviceItem.tsx
@@ -15,7 +15,7 @@ function DeviceItem({ device }: { device: IDevice }) {
   let imagePath;
   if (device.type === "browser") {
     const browser = device.browser.split(" ")[0];
-    imagePath = browserType[browser];
+    imagePath = browserType[browser] || "Chrome";
   } else {
     const os = device.device;
     imagePath = osType[os];
